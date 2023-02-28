254831c7c2a7e0d6e3358d2872b12e5777273044
Nathaniel Alcala <semblancewarden@Gmail.com>
2023-02-28 23:41:11 +0800

    ✨ Added 2 new systems
    1. BuildHandlerSimple - A building handler with a 2 pass condition checking system and simple usage setup
    
    2. SimpleInventoryHandler - a simple inventory management system with basic functionality and item to context updating. (This script is untested and still needs work)

---
+++ UnscriptedLogic/Others/BuildHandlerSimple.cs
@@ -0,0 +1,154 @@
+﻿using UnityEngine;
+
+namespace UnscriptedLogic.Builders
+{
+    public interface IBuildable
+    {
+        void LocalPassBuildConditions<T>(T builder, out List<LocalBuildCondition> localBuildConditions);
+    }
+
+    public interface IBuilder<TBuildable, TBuildableContainer> where TBuildable : IBuildable
+    {
+        TBuildableContainer[] buildableContainers { get; }
+
+        TBuildable WhenGetBuildable(TBuildableContainer buildableObject);
+        void WhenCreateBuildable(int index, Vector3 position, Quaternion rotation, TBuildableContainer buildableContainer);
+        void OnConditionResult(BuildResult buildResult);
+    }
+
+
+    public struct BuildResult
+    {
+        private bool passed;
+        private string description;
+
+        public bool Passed => passed;
+        public string Description => description;
+
+        public BuildResult(bool passed, string description)
+        {
+            this.passed = passed;
+            this.description = description;
+        }
+    }
+
+    public class AdminBuildCondition<T>
+    {
+        private string name;
+        private Predicate<T> condition;
+        private string onFailConditionText;
+        private string onPassConditionText;
+
+        public string Name => name;
+        public Predicate<T> Condition => condition;
+        public string OnFailConditionText => onFailConditionText;
+        public string OnPassConditionText => onPassConditionText;
+
+        public AdminBuildCondition(string name, Predicate<T> condition, string onFailConditionText = "", string onPassConditionText = "")
+        {
+            this.name = name;
+            this.condition = condition;
+            this.onFailConditionText = onFailConditionText;
+            this.onPassConditionText = onPassConditionText;
+        }
+    }
+
+    public class LocalBuildCondition
+    {
+        public delegate bool LocalCondition(Vector3 position, Quaternion rotation);
+
+        private string name;
+        private LocalCondition condition;
+        private string onFailConditionText;
+        private string onPassConditionText;
+
+        public string Name => name;
+        public LocalCondition Condition => condition;
+        public string FailConditionText => onFailConditionText;
+        public string PassConditionText => onPassConditionText;
+
+        public LocalBuildCondition(string name, LocalCondition condition, string onFailConditionText, string onPassConditionText)
+        {
+            this.name = name;
+            this.condition = condition;
+            this.onFailConditionText = onFailConditionText;
+            this.onPassConditionText = onPassConditionText;
+        }
+    }
+
+    public class BuildHandlerSimple<TBuildable, Builder, BuildableContainer> where TBuildable : IBuildable where Builder : IBuilder<TBuildable, BuildableContainer>
+    {
+        public delegate GameObject BuildablePreview(int index, Vector3 position, Quaternion rotation);
+        public BuildablePreview? CreateBuildablePreview;
+        public List<AdminBuildCondition<TBuildable>> adminBuildConditions;
+        public List<BuildableContainer> buildableObjects;
+        private Builder builder;
+
+        public BuildHandlerSimple(Builder builder, List<BuildableContainer> buildableObjects)
+        {
+            adminBuildConditions = new List<AdminBuildCondition<TBuildable>>();
+            this.builder = builder;
+            this.buildableObjects = buildableObjects;
+        }
+
+        public bool AdminConditionCheck(TBuildable buildable, Action<BuildResult> OnConditionResult)
+        {
+            for (int i = 0; i < adminBuildConditions.Count; i++)
+            {
+                if (!adminBuildConditions[i].Condition(buildable))
+                {
+                    BuildResult failedBuildResult = new BuildResult(false, "Admin Condition " + (i + 1) + ": " + adminBuildConditions[i].Name + " failed.");
+                    OnConditionResult?.Invoke(failedBuildResult);
+                    return false;
+                }
+            }
+
+            BuildResult passedBuildResult = new BuildResult(true, "Admin conditions passed.");
+            OnConditionResult?.Invoke(passedBuildResult);
+            return true;
+        }
+
+        public bool LocalConditionCheck(TBuildable buildable, Vector3 position, Quaternion rotation, Action<BuildResult> OnConditionResult)
+        {
+            buildable.LocalPassBuildConditions(builder, out List<LocalBuildCondition> localBuildConditions);
+
+            for (int i = 0; i < localBuildConditions.Count; i++)
+            {
+                if (!localBuildConditions[i].Condition(position, rotation))
+                {
+                    BuildResult failedBuildResult = new BuildResult(false, "Local Condition " + (i + 1) + ": " + localBuildConditions[i].Name + " failed.");
+                    OnConditionResult?.Invoke(failedBuildResult);
+                    return false;
+                }
+            }
+
+            BuildResult passedBuildResult = new BuildResult(true, "Local conditions passed!");
+            OnConditionResult?.Invoke(passedBuildResult);
+            return true;
+        }
+
+        public void Build(int index, Vector3 position, Quaternion rotation, Action<BuildResult> BuildResult, bool doAdminPass = true, bool doLocalPass = true)
+        {
+            TBuildable buildable = builder.WhenGetBuildable(buildableObjects[index]);
+
+            if (doAdminPass)
+            {
+                if (!AdminConditionCheck(buildable, BuildResult))
+                {
+                    return;
+                }
+            }
+
+            if (doLocalPass)
+            {
+                if (!LocalConditionCheck(buildable, position, rotation, BuildResult))
+                {
+                    return;
+                }
+            }
+
+            builder.WhenCreateBuildable(index, position, rotation, buildableObjects[index]);
+        }
+    }
+
+}
